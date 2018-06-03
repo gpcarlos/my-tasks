566a94404c155be744cb0607053747218eef4a47
Carlos Gallardo Polanco <carlosgallardopolanco@gmail.com>
2018-06-03 12:25:16 +0200

    Added the exercises: - Objects - Case Classes - Traits - Tuples - Literals - Options

---
+++ src/main/scala/exercises/types/caseclasses.scala
@@ -0,0 +1,11 @@
+package exercises.types
+
+object caseclasses {
+
+  // Classes with powers!!!!
+
+  case class Dog(name: String, breed: String)
+
+  case class Person(name: String, age: Int = 0, ssn: String = "")
+
+}
