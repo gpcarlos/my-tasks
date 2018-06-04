fbefb4920d9d7c4b9cd0c3d15c3ac5923e209f78
Carlos Gallardo Polanco <carlosgallardopolanco@gmail.com>
2018-06-05 00:09:04 +0200

    Minnor change in Challenge 3

--- src/main/scala/challenges/challenge3.scala
+++ src/main/scala/challenges/challenge3.scala
@@ -3,6 +3,6 @@ package challenges
 object challenge3 {
 
   def combinationsWithoutSecures (total: Int, addend: Int, secure: Set[Int] = Set.empty): List[List[Int]] =
-    (1 to 9 toList).combinations(addend).filter(v => v.sum == total && secure.subsetOf(v.toSet)).map(_.toSet.diff(secure).toList).toList
+    (1 to 9 toList).combinations(addend).filter(v => v.sum == total && secure.subsetOf(v.toSet)).map(_.diff(secure.toList)).toList
 
 }
