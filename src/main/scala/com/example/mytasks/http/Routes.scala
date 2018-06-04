1415cfe0c1b596ddb3fb5238c8cf4c448fb09f44
Carlos Gallardo Polanco <carlosgallardopolanco@gmail.com>
2018-06-04 19:47:15 +0200

    Added another test case

---
+++ src/main/scala/com/example/mytasks/http/Routes.scala
@@ -0,0 +1,14 @@
+package com.example.mytasks.http
+
+import cats.effect.Effect
+import org.http4s.HttpService
+import org.http4s.dsl.Http4sDsl
+
+class Routes[F[_]: Effect] extends Http4sDsl[F] {
+
+  var service: HttpService[F] = HttpService[F] {
+
+    case GET -> Root => Ok("Hola")
+  }
+
+}
